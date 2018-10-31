e079f9b6b89fbb78ba1322d5c55b690a016b8799
lauhaitao <lauhaitao@outlook.com>
2018-10-31 11:34:38 +0800

    no message

--- project1/src/router/index.js
+++ project1/src/router/index.js
@@ -4,85 +4,70 @@ import City from '../components/1-Takeaway/component/city'
 import SearchPath from '../components/1-Takeaway/component/search'
 import Takeaway from '../components/1-Takeaway/takeaway'
 import Order from '../components/home/order'
+import Search from '../components/home/search'
 import Search3 from '../components/components/search3'
 import Mine from '../components/home/mine'
 import Balance from '../components/home/balance'
-import Benefit from '../components/1-Takeaway/benefit'
+import Benefit from '../components/home/benefit'
 import Points from '../components/home/points'
 import Wd from '../components/home/wd'
 import Login from '../components/home/login'
 import VipCard from "../components/home/vipCard"
 import DownLoad from "../components/home/download"
 import Service from "../components/home/service"
-import Questiondetail from "../components/home/questiondetail"
-import Home from '../components/home/home'
+import  Questiondetail from "../components/home/questiondetail"
+import  Home from '../components/home/home'
+import Hd from '../../src/components/components/hd'
 import Mlt from "../components/components/mlt"
 import Cxg from "../components/components/cxg"
 import Bzzp from '../components/components/bzzp'
-<<<<<<< HEAD
 import Shopdetail from '../components/components/shopdetail'
 import Swiper from '../components/components/swiper'
-=======
-import Critic from '../components/components/critic'
->>>>>>> 47ab1e2ee1160f441a3834ddc0bc608776052f25
 import LoginDetail from '../components/home/loginDetail'
-import SetUsername from '../components/home/setusername'
-import Address from '../components/home/address'
-import Add from '../components/home/add'
-import AddDetail from '../components/home/adddetail'
+import SetUsername  from '../components/home/setusername'
+import Address  from '../components/home/address'
+import Add  from '../components/home/add'
+import AddDetail  from '../components/home/adddetail'
 import Forget from '../components/home/forget'
-<<<<<<< HEAD
 import Dpdetail from '../components/components/dpdetail'
 import Shopsafe from '../components/components/shopsafe'
-import Zsd from '../components/components/zsd'
 
-=======
-import Hongbao from '../components/1-Takeaway/hongbao'
-import Daijinquan from '../components/1-Takeaway/daijinquan'
-import PayLine from '../components/home/payline'
-import VipDescription from '../components/home/vipdescription'
-import Record from '../components/home/record'
-import UseCard from '../components/home/usecard'
-import Shopdetail from '../components/components/shopdetail'
-import Dpdetail from '../components/components/dpdetail'
-import Shopsafe from '../components/components/shopsafe'
->>>>>>> 47ab1e2ee1160f441a3834ddc0bc608776052f25
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
-{
-  path:"/zsd",
-  component:Zsd
-}
-    ,
     {
-      path: "/",
-      component: City
+      path: '/head',
+      name: 'hd',
+      component: Hd
     },
     {
-      path: '/search',
+      path:"/",
+      component:City
+    },
+    {
+      path: '/city/:id',
       name: 'search',
       component: SearchPath
     },
     {
       path: '/takeaway',
-      component: Home,
-      children: [
+      component:Home,
+      children:[
         {
-          path: "/search3",
-          component: Search3,
-          name: "search3"
+          path:"/search3",
+          component:Search3,
+          name:"search3"
         },
         {
-          path: "/order",
-          name:'order',
-          component: Order
+          path:"/order",
+          component:Order
         },
         {
-          path: "/mine",
-          component: Mine,
-          name: 'mine'
+          path:"/mine",
+          component:Mine,
+          name:'mine'
         },
         {
           path: '/takeaway',
@@ -92,32 +77,23 @@ export default new Router({
       ]
     },
     {
-      path: "/balance",
-      component: Balance
+      path:"/search",
+      component:Search,
     },
     {
-      path: "/benefit",
-      component: Benefit,
-      children: [{
-        path: "/benefit/hongbao",
-        component: Hongbao
-      }, {
-        path: "/benefit/daijinquan",
-        component: Daijinquan
-      }]
+      path:"/balance",
+      component:Balance
     },
     {
-      path: "/points",
-      name: "points",
-      component: Points
+      path:"/benefit",
+      component:Benefit
     },
     {
-      path: "/wd",
-      name: "wd",
-      component: Wd
+      path:"/points",
+      name:"points",
+      component:Points
     },
     {
-<<<<<<< HEAD
       path:"/swiper",
       name:"swiper",
       component:Swiper
@@ -126,34 +102,34 @@ export default new Router({
       path:"/wd",
       name:"wd",
       component:Wd
-=======
-      path: "/login",
-      component: Login
->>>>>>> 47ab1e2ee1160f441a3834ddc0bc608776052f25
     },
     {
-      path: "/vipcard",
-      component: VipCard
+      path:"/login",
+      component:Login
     },
     {
-      path: "/download",
-      component: DownLoad
+      path:"/vipcard",
+      component:VipCard
     },
     {
-      path: '/service',
-      component: Service,
+      path:"/download",
+      component:DownLoad
     },
     {
-      path: '/service/questiondetail',
-      name: 'questiondetail',
-      component: Questiondetail
+      path:'/service',
+      component:Service,
+    },
+    {
+      path:'/service/questiondetail',
+      name:'questiondetail',
+      component:Questiondetail
     }, {
       path: '/mlt/:id',
       name: 'mlt',
       component: Mlt
     },
     {
-      path: "/cxg/:id?",
+    path: "/cxg/:id?",
       name: "cxg",
       component: Cxg
     }, {
@@ -167,12 +143,11 @@ export default new Router({
       component: Forget
     },
     {
-      path: "/critic",
-      component: Critic,
-      name: "critic"
+      path:"/shopdetail",
+      name:"shopdetail",
+      component:Shopdetail
     },
     {
-<<<<<<< HEAD
       path:"/shopdetail/dpdetail",
       name:"dpdetail",
       component:Dpdetail
@@ -181,60 +156,28 @@ export default new Router({
 path:"/shopdetail/dpdetail/shopsafe",
 name:"shopsafe",
 component:Shopsafe
-=======
-      path: '/mine/logindetail',
-      name: 'logindetail',
-      component: LoginDetail
->>>>>>> 47ab1e2ee1160f441a3834ddc0bc608776052f25
     },
     {
-      path: '/setusername',
-      component: SetUsername
+      path:'/mine/logindetail',
+      name:'logindetail',
+      component:LoginDetail
     },
     {
-      path: '/address',
-      name: 'address',
-      component: Address
+      path:'/setusername',
+      component:SetUsername
     },
     {
-      path: '/address/add',
-      name: 'add',
-      component: Add
+      path:'/address',
+      component:Address
     },
     {
-      path: '/add/adddetail',
-      component: AddDetail
+      path:'/address/add',
+      component:Add
     },
     {
-      path: '/payline',
-      component: PayLine
+      path:'/add/adddetail',
+      component:AddDetail
     },
-    {
-      path: '/vipcard/vipdesciption',
-      component: VipDescription
-    },
-    {
-      path: '/vipcard/record',
-      component: Record
-    },
-    {
-      path: '/vipcard/usecard',
-      component: UseCard
-    },
-    {
-      path: "/shopdetail",
-      name: "shopdetail",
-      component: Shopdetail
-    },
-    {
-      path: "/shopdetail/dpdetail",
-      name: "dpdetail",
-      component: Dpdetail
-    },
-    {
-      path: "/shopdetail/dpdetail/shopsafe",
-      name: "shopsafe",
-      component: Shopsafe
-    },
+
   ]
-});
+})
